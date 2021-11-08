614cad8211f1d0885dd896a52bd27009b3d94765
Manuel Sanchez Del Rio <srio@esrf.eu>
2021-11-08 12:45:35 +0100

    updated Shadow calculator

--- notebooks/demo_pyvinyl_shadow3.py
+++ notebooks/demo_pyvinyl_shadow3.py
@@ -10,38 +10,72 @@ from Shadow3Calculator import Shadow3Calculator
 calculator  = Shadow3Calculator("")
 
 # ### Setup the parameters
-calculator.setParams()
+calculator.setParams(number_of_optical_elements=1)
+
+print(">>>>>DEFAULTS: ", calculator.parameters, "\n\n\n")
+
+calculator.parameters["oe0.FDISTR"].value = 3
+calculator.parameters["oe0.F_COLOR"].value = 3
+calculator.parameters["oe0.F_PHOT"].value = 0
+calculator.parameters["oe0.HDIV1"].value = 0.0
+calculator.parameters["oe0.HDIV2"].value = 0.0
+calculator.parameters["oe0.NPOINT"].value = 10000
+calculator.parameters["oe0.PH1"].value = 8799.999
+calculator.parameters["oe0.PH2"].value = 8799.999
+calculator.parameters["oe0.SIGDIX"].value = 4.728541797631135e-06
+calculator.parameters["oe0.SIGDIZ"].value = 4.095010077148124e-06
+calculator.parameters["oe0.SIGMAX"].value = 0.0015810951361940363
+calculator.parameters["oe0.SIGMAZ"].value = 0.0006681031579752021
+calculator.parameters["oe0.VDIV1"].value = 0.0
+calculator.parameters["oe0.VDIV2"].value = 0.0
+
+calculator.parameters["oe1.DUMMY"].value = 1.0
+calculator.parameters["oe1.FMIRR"].value = 3
+calculator.parameters["oe1.FWRITE"].value = 1
+calculator.parameters["oe1.T_IMAGE"].value = 1000.0
+calculator.parameters["oe1.T_INCIDENCE"].value = 89.828
+calculator.parameters["oe1.T_REFLECTION"].value = 89.828
+calculator.parameters["oe1.T_SOURCE"].value = 4000.0
+
 
 ### Run the backengine
 
 calculator.backengine()
 
+import Shadow
+Shadow.ShadowTools.plotxy(calculator.data, 1, 3, nbins=101, nolost=1, title="Real space")
+
 ### Look at the data and store as hdf5
-# calculator.data
-# calculator.saveH5(calculator.output_path)
+print(calculator.data)
+calculator.saveH5("tmp.h5")
 
-### Save the parameters to a human readable json file.
-calculator.parameters.to_json("my_parameters.json")
 
-### Save calculator to binary dump.
-# dumpfile = calculator.dump()
 
-########################################################################################################################
+if False: # errors here!!!!
 
-### Load back parameters
+    ### Save the parameters to a human readable json file.
+    print(calculator.parameters["oe1.CCC"])
+    calculator.parameters.to_json("my_parameters.json")
 
-new_parameters = Parameters.from_json("my_parameters.json")
+    ### Save calculator to binary dump.
+    # dumpfile = calculator.dump()
 
-new_parameters["NPOINT"] = 10000
+    ########################################################################################################################
 
-print('NPOINT:', new_parameters['NPOINT'])
+    ### Load back parameters
 
-new_calculator  = Shadow3Calculator("", output_path="out.h5")
+    new_parameters = Parameters.from_json("my_parameters.json")
 
-new_calculator.setParams(native=new_parameters)
+    new_parameters["oe0.NPOINT"] = 10000
 
-new_calculator.backengine()
+    print('oe0.NPOINT:', new_parameters['oe0.NPOINT'])
 
-new_calculator.saveH5("tmp.h5")
+    new_calculator  = Shadow3Calculator("", output_path="out.h5")
+
+    new_calculator.setParams(native=new_parameters)
+
+    new_calculator.backengine()
+
+    new_calculator.saveH5("tmp.h5")
 
 
--- tests/Shadow3Calculator.py
+++ tests/Shadow3Calculator.py
@@ -1,6 +1,6 @@
 from libpyvinyl.BaseCalculator import BaseCalculator, CalculatorParameters
 import Shadow
-
+import inspect
 
 class Shadow3Calculator(BaseCalculator):
     def __init__(self,
@@ -15,9 +15,12 @@ class Shadow3Calculator(BaseCalculator):
                          dumpfile=dumpfile,
                          output_path=output_path)
 
+        self.number_of_optical_elements = 0
+
     def setParams(self,
             source=None,
             beamline=[],
+            number_of_optical_elements = 0,
             native=None,
             ):
 
@@ -38,129 +41,87 @@ class Shadow3Calculator(BaseCalculator):
             self.parameters = CalculatorParameters()
 
         for key in oe0_dict.keys():
-            print(key, oe0_dict[key])
-            self.parameters.new_parameter(key)
+            self.parameters.new_parameter("oe0."+key)
             if isinstance(oe0_dict[key], bytes):
-                self.parameters[key].set_value(oe0_dict[key].decode("utf-8"))
+                self.parameters["oe0."+key].set_value(oe0_dict[key].decode("utf-8"))
             else:
-                self.parameters[key].set_value(oe0_dict[key])
+                self.parameters["oe0."+key].set_value(oe0_dict[key])
 
-        # beamline TODO!
+        # beamline
+        if len(beamline) == 0 and number_of_optical_elements > 0:
+            beamline = [Shadow.OE()] * number_of_optical_elements
+
+        for i in range(len(beamline)):
+            if isinstance(beamline[i], Shadow.OE):
+                oe_i = beamline[i]
+            else:
+                oe_i = Shadow.OE()
+
+            oe_i_dict = oe_i.to_dictionary()
+
+            if not isinstance(self.parameters, CalculatorParameters):
+                self.parameters = CalculatorParameters()
+
+            for key in oe_i_dict.keys():
+                self.parameters.new_parameter("oe%d.%s" % (i+1, key))
+                if isinstance(oe_i_dict[key], bytes):
+                    self.parameters["oe%d.%s" % (i+1, key)].set_value(oe_i_dict[key].decode("utf-8"))
+                else:
+                    self.parameters["oe%d.%s" % (i+1, key)].set_value(oe_i_dict[key])
+
+        self.number_of_optical_elements = number_of_optical_elements
+
+    def __get_valiable_list(self, object1):
+        """
+        returns a list of the Shadow.Source or Shadow.OE variables
+        """
+        mem = inspect.getmembers(object1)
+        mylist = []
+        for i,var in enumerate(mem):
+            if var[0].isupper():
+                mylist.append(var[0])
+        return(mylist)
 
     def backengine(self):
         beam = Shadow.Beam()
         oe0 = Shadow.Source()
 
-        oe0.FDISTR            = self.parameters["FDISTR"].value
-        oe0.FGRID             = self.parameters["FGRID"].value
-        oe0.FSOUR             = self.parameters["FSOUR"].value
-        oe0.FSOURCE_DEPTH     = self.parameters["FSOURCE_DEPTH"].value
-        oe0.F_COHER           = self.parameters["F_COHER"].value
-        oe0.F_COLOR           = self.parameters["F_COLOR"].value
-        oe0.F_PHOT            = self.parameters["F_PHOT"].value
-        oe0.F_POL             = self.parameters["F_POL"].value
-        oe0.F_POLAR           = self.parameters["F_POLAR"].value
-        oe0.F_OPD             = self.parameters["F_OPD"].value
-        oe0.F_WIGGLER         = self.parameters["F_WIGGLER"].value
-        oe0.F_BOUND_SOUR      = self.parameters["F_BOUND_SOUR"].value
-        oe0.F_SR_TYPE         = self.parameters["F_SR_TYPE"].value
-        oe0.ISTAR1            = self.parameters["ISTAR1"].value
-        oe0.NPOINT            = self.parameters["NPOINT"].value
-        oe0.NCOL              = self.parameters["NCOL"].value
-        oe0.N_CIRCLE          = self.parameters["N_CIRCLE"].value
-        oe0.N_COLOR           = self.parameters["N_COLOR"].value
-        oe0.N_CONE            = self.parameters["N_CONE"].value
-        oe0.IDO_VX            = self.parameters["IDO_VX"].value
-        oe0.IDO_VZ            = self.parameters["IDO_VZ"].value
-        oe0.IDO_X_S           = self.parameters["IDO_X_S"].value
-        oe0.IDO_Y_S           = self.parameters["IDO_Y_S"].value
-        oe0.IDO_Z_S           = self.parameters["IDO_Z_S"].value
-        oe0.IDO_XL            = self.parameters["IDO_XL"].value
-        oe0.IDO_XN            = self.parameters["IDO_XN"].value
-        oe0.IDO_ZL            = self.parameters["IDO_ZL"].value
-        oe0.IDO_ZN            = self.parameters["IDO_ZN"].value
-        oe0.SIGXL1            = self.parameters["SIGXL1"].value
-        oe0.SIGXL2            = self.parameters["SIGXL2"].value
-        oe0.SIGXL3            = self.parameters["SIGXL3"].value
-        oe0.SIGXL4            = self.parameters["SIGXL4"].value
-        oe0.SIGXL5            = self.parameters["SIGXL5"].value
-        oe0.SIGXL6            = self.parameters["SIGXL6"].value
-        oe0.SIGXL7            = self.parameters["SIGXL7"].value
-        oe0.SIGXL8            = self.parameters["SIGXL8"].value
-        oe0.SIGXL9            = self.parameters["SIGXL9"].value
-        oe0.SIGXL10           = self.parameters["SIGXL10"].value
-        oe0.SIGZL1            = self.parameters["SIGZL1"].value
-        oe0.SIGZL2            = self.parameters["SIGZL2"].value
-        oe0.SIGZL3            = self.parameters["SIGZL3"].value
-        oe0.SIGZL4            = self.parameters["SIGZL4"].value
-        oe0.SIGZL5            = self.parameters["SIGZL5"].value
-        oe0.SIGZL6            = self.parameters["SIGZL6"].value
-        oe0.SIGZL7            = self.parameters["SIGZL7"].value
-        oe0.SIGZL8            = self.parameters["SIGZL8"].value
-        oe0.SIGZL9            = self.parameters["SIGZL9"].value
-        oe0.SIGZL10           = self.parameters["SIGZL10"].value
-        oe0.CONV_FACT         = self.parameters["CONV_FACT"].value
-        oe0.CONE_MAX          = self.parameters["CONE_MAX"].value
-        oe0.CONE_MIN          = self.parameters["CONE_MIN"].value
-        oe0.EPSI_DX           = self.parameters["EPSI_DX"].value
-        oe0.EPSI_DZ           = self.parameters["EPSI_DZ"].value
-        oe0.EPSI_X            = self.parameters["EPSI_X"].value
-        oe0.EPSI_Z            = self.parameters["EPSI_Z"].value
-        oe0.HDIV1             = self.parameters["HDIV1"].value
-        oe0.HDIV2             = self.parameters["HDIV2"].value
-        oe0.PH1               = self.parameters["PH1"].value
-        oe0.PH2               = self.parameters["PH2"].value
-        oe0.PH3               = self.parameters["PH3"].value
-        oe0.PH4               = self.parameters["PH4"].value
-        oe0.PH5               = self.parameters["PH5"].value
-        oe0.PH6               = self.parameters["PH6"].value
-        oe0.PH7               = self.parameters["PH7"].value
-        oe0.PH8               = self.parameters["PH8"].value
-        oe0.PH9               = self.parameters["PH9"].value
-        oe0.PH10              = self.parameters["PH10"].value
-        oe0.RL1               = self.parameters["RL1"].value
-        oe0.RL2               = self.parameters["RL2"].value
-        oe0.RL3               = self.parameters["RL3"].value
-        oe0.RL4               = self.parameters["RL4"].value
-        oe0.RL5               = self.parameters["RL5"].value
-        oe0.RL6               = self.parameters["RL6"].value
-        oe0.RL7               = self.parameters["RL7"].value
-        oe0.RL8               = self.parameters["RL8"].value
-        oe0.RL9               = self.parameters["RL9"].value
-        oe0.RL10              = self.parameters["RL10"].value
-        oe0.BENER             = self.parameters["BENER"].value
-        oe0.POL_ANGLE         = self.parameters["POL_ANGLE"].value
-        oe0.POL_DEG           = self.parameters["POL_DEG"].value
-        oe0.R_ALADDIN         = self.parameters["R_ALADDIN"].value
-        oe0.R_MAGNET          = self.parameters["R_MAGNET"].value
-        oe0.SIGDIX            = self.parameters["SIGDIX"].value
-        oe0.SIGDIZ            = self.parameters["SIGDIZ"].value
-        oe0.SIGMAX            = self.parameters["SIGMAX"].value
-        oe0.SIGMAY            = self.parameters["SIGMAY"].value
-        oe0.SIGMAZ            = self.parameters["SIGMAZ"].value
-        oe0.VDIV1             = self.parameters["VDIV1"].value
-        oe0.VDIV2             = self.parameters["VDIV2"].value
-        oe0.WXSOU             = self.parameters["WXSOU"].value
-        oe0.WYSOU             = self.parameters["WYSOU"].value
-        oe0.WZSOU             = self.parameters["WZSOU"].value
-        oe0.PLASMA_ANGLE      = self.parameters["PLASMA_ANGLE"].value
-        oe0.FILE_TRAJ         = bytes(self.parameters["FILE_TRAJ"].value, 'UTF-8')
-        oe0.FILE_SOURCE       = bytes(self.parameters["FILE_SOURCE"].value, 'UTF-8')
-        oe0.FILE_BOUND        = bytes(self.parameters["FILE_BOUND"].value, 'UTF-8')
-        oe0.OE_NUMBER         = self.parameters["OE_NUMBER"].value
-        oe0.NTOTALPOINT       = self.parameters["NTOTALPOINT"].value
-        oe0.IDUMMY            = self.parameters["IDUMMY"].value
-        oe0.DUMMY             = self.parameters["DUMMY"].value
-        oe0.F_NEW             = self.parameters["F_NEW"].value
+
+        oe0_list = self.__get_valiable_list(oe0)
+
+        for name in oe0_list:
+            try:
+                value = self.parameters["oe0."+name].value
+                if isinstance(value, str):
+                    value = bytes(value, 'UTF-8')
+                setattr(oe0, name, value)
+            except:
+                raise Exception("Error setting parameters name %s" % name)
+
 
         beam.genSource(oe0)
+
+        if self.number_of_optical_elements > 0:
+            oei_list = self.__get_valiable_list(Shadow.OE())
+            for i in range(self.number_of_optical_elements):
+                oe_i = Shadow.OE()
+                for name in oei_list:
+                    try:
+                        value = self.parameters["oe%d.%s" % (i+1, name)].value
+                        if isinstance(value, str):
+                            value = bytes(value, 'UTF-8')
+                        setattr(oe_i, name, value)
+                    except:
+                        raise Exception("Error setting parameters name %s" % name)
+
+                beam.traceOE(oe_i, i+1)
 
         self._set_data(beam)
         return 0
 
 
-    def dump(self): # overwritten method
-        self.data.write("begin.dat")
+    def dump(self,filename="star.01"): # overwritten method
+        self.data.write(filename)
 
     def saveH5(self, filename="tmp.h5", openpmd=False):
         from orangecontrib.panosc.shadow.util.openPMD import saveShadowToHDF
